a4e26edb77f7b07424a079a65ad46f0f3ff3b087
Dhamu785 <dhamodharan1888@gmail.com>
2024-12-19 18:19:31 +0530

    adding double conv2D

--- 01_U-net/PyTorch/01_Uygar Kurt/01_model.py
+++ 01_U-net/PyTorch/01_Uygar Kurt/01_model.py
@@ -5,6 +5,21 @@ import os
 import shutil
 import numpy as np
 import matplotlib.pyplot as plt
+import torch.nn as nn
 
 device = 'cuda' if t.cuda.is_available() else 'cpu'
 # %%
+ 
+
+class DoubleConv:
+    def __init__(self, in_channel, out_channels):
+        super().__init__()
+        self.conv_op = nn.Sequential(
+            nn.Conv2d(in_channel, out_channels, kernel_size=(3,3), padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=(3,3), padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv_op(x)
