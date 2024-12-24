c630303266f2ddb159ceff7b2afd4fc5056d908e
Dhamu785 <dhamodharan1888@gmail.com>
2024-12-24 18:16:08 +0530

    unet overview architecture

--- 01_U-net/PyTorch/01_Uygar Kurt/unet.py
+++ 01_U-net/PyTorch/01_Uygar Kurt/unet.py
@@ -2,3 +2,35 @@ import torch as t
 import torch.nn as nn
 
 from unet_parts import DoubleConv, Downsample, UpSample
+
+class unet(nn.Module):
+    def __init__(self, in_channel, num_classes):
+        super().__init__()
+        self.down_conv1 = Downsample(in_channel=in_channel, out_channel=64)
+        self.down_conv2 = Downsample(in_channel=64, out_channel=128)
+        self.down_conv3 = Downsample(in_channel=128, out_channel=256)
+        self.down_conv4 = Downsample(in_channel=256, out_channel=512)
+
+        self.bottble_neck = DoubleConv(in_channel=512, out_channels=1024)
+
+        self.up_conv1 = UpSample(in_channel=1024, out_channel=512)
+        self.up_conv2 = UpSample(in_channel=512, out_channel=256)
+        self.up_conv3 = UpSample(in_channel=256, out_channel=128)
+        self.up_conv4 = UpSample(in_channel=128, out_channel=64)
+
+        self.out = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=(1,1))
+
+    def forward(self, x):
+        conv1, p1 = self.down_conv1(x)
+        conv2, p2 = self.down_conv2(p1)
+        conv3, p3 = self.down_conv3(p2)
+        conv4, p4 = self.down_conv4(p3)
+        
+        btl_nk = self.bottble_neck(p4)
+
+        up1 = self.up_conv1(btl_nk, conv4)
+        up2 = self.up_conv2(up1, conv3)
+        up3 = self.up_conv3(up2, conv2)
+        up4 = self.up_conv4(up3, conv1)
+
+        out = self.out(up4)
