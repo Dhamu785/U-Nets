b1f4fabd3be0790aacf27276c6d0d76e31015ebb
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-01-31 19:22:31 +0530

    create dataset

--- 01_U-net/PyTorch/02_Digital_srini/04_multi_class.py
+++ 01_U-net/PyTorch/02_Digital_srini/04_multi_class.py
@@ -1,2 +1,9 @@
 from sklearn.utils import class_weight
 # np.bincount
+from torch.utils.data.dataset import Dataset
+from torchvision import transforms
+
+class DataSet(Dataset):
+    def __init__(self, path_train, path_test):
+        self.path_train = path_train
+        self.path_test = path_test
