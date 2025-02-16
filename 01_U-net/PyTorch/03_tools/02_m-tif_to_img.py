9a21e197635863c814281a024f6c043c4abb69b1
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-02-17 00:12:25 +0530

    tif to jpg

--- 01_U-net/PyTorch/03_tools/02_m-tif_to_img.py
+++ 01_U-net/PyTorch/03_tools/02_m-tif_to_img.py
@@ -2,12 +2,15 @@
 from PIL import Image 
 
 # %% init path
+# Path for windows
 # target_path_msk = "C:\\Users\\dhamu\\Documents\\Python all\\torch_works\\01\\00_datasets-from_online\\01_Segmentations\\02_Instance\\02\\sandstone_data_for_ML\\data_for_3D_Unet\\train_masks"
 # tiff_msk = "C:\\Users\\dhamu\\Documents\\Python all\\torch_works\\01\\00_datasets-from_online\\01_Segmentations\\02_Instance\\02\\sandstone_data_for_ML\\data_for_3D_Unet\\train_masks_256_256_256.tif"
 # tiff_img = "C:\\Users\\dhamu\\Documents\\Python all\\torch_works\\01\\00_datasets-from_online\\01_Segmentations\\02_Instance\\02\\sandstone_data_for_ML\\data_for_3D_Unet\\train_images_256_256_256.tif"
 # target_path_img = "C:\\Users\\dhamu\\Documents\\Python all\\torch_works\\01\\00_datasets-from_online\\01_Segmentations\\02_Instance\\02\\sandstone_data_for_ML\\data_for_3D_Unet\\train_img"
-target_path_msk = '/Users/dhamodharan/My-Python/AI-Tutorials/01_Dataset from online/sandstone_data_for_ML/01_Segmentation/02_Instance segmentation/data_for_3D_Unet/train_masks'
-tiff_msk = '/Users/dhamodharan/My-Python/AI-Tutorials/01_Dataset from online/sandstone_data_for_ML/01_Segmentation/02_Instance segmentation/data_for_3D_Unet/train_masks/train_masks_256_256_256.tif'
+
+# Path for mac
+target_path_msk = '/Users/dhamodharan/My-Python/AI-Tutorials/01_Dataset from online/sandstone_data_for_ML/01_Segmentation/02_Instance segmentation/data_for_3D_Unet/target_masks/'
+tiff_msk = '/Users/dhamodharan/My-Python/AI-Tutorials/01_Dataset from online/sandstone_data_for_ML/01_Segmentation/02_Instance segmentation/data_for_3D_Unet/train_masks_256_256_256.tif'
 
 # %%
 tiff_img = Image.open(tiff_msk)
