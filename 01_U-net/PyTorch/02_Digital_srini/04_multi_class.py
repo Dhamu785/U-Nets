cdbbefd8ef4e07e96f32ac844e31a603e6548d9f
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-02-19 23:44:52 +0530

    create dataset for multiclass segmentation

--- 01_U-net/PyTorch/02_Digital_srini/04_multi_class.py
+++ 01_U-net/PyTorch/02_Digital_srini/04_multi_class.py
@@ -2,8 +2,22 @@ from sklearn.utils import class_weight
 # np.bincount
 from torch.utils.data.dataset import Dataset
 from torchvision import transforms
+import os
 
 class DataSet(Dataset):
-    def __init__(self, path_train, path_test):
+    def __init__(self, path_train, path_mask, transforms=None):
         self.path_train = path_train
-        self.path_test = path_test
+        self.path_mask = path_mask
+        self.transform = transforms
+        self.images = os.listdir(self.path_train)
+        self.masks = os.listdir(self.path_mask)
+        if transforms != None:
+            self.transform = transforms
+        else:
+            self.transform = transforms
+    def __len__(self):
+        return len(self.masks)
+    
+    def __getitem__(self, index):
+        s_img = os.path.join(self.path_train, self.images[index])
+        s_msk = os.path.join(self.path_mask, self.masks[index])
