c0235978ec6840bbf5560c49722b596397384083
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-02-23 22:45:39 +0530

    transpose used for c,h,w purpose

---
+++ 01_U-net/PyTorch/02_Digital_srini/04.1_onehot.py
@@ -0,0 +1,38 @@
+# %% lib imports
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+import numpy as np
+
+# %% read using pillow
+path_msk = '/Users/dhamodharan/My-Python/AI-Tutorials/dataset/Types of seg/02_Multi/Y/img_1.png'
+pil_img = Image.open(path_msk)
+print(pil_img)
+print(np.unique(np.array(pil_img)))
+img_data = cv2.imread(path_msk, 0)
+print(np.unique(img_data))
+
+# %% Label encoder + onehot encoder test
+import torch as t
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from torchvision import transforms
+
+sample = np.array([['cat', 'dog', 'fish'],['cat', 'fish', 'fish']]).reshape(-1)
+print(f"Samples shape = {sample}")
+labels = LabelEncoder()
+lbl = labels.fit_transform(sample)
+print(lbl)
+# %%
+onehot = t.nn.functional.one_hot(t.tensor(lbl), 3)
+print(onehot)
+# %%
+print(onehot.reshape(2,3, -1))
+print(onehot.reshape(2,3, -1).shape)
+# %%
+print(onehot.reshape(-1,2,3))
+
+np.transpose(onehot.reshape(2,3,-1), (2,0,1))
+# %%
+np.transpose(onehot.reshape(2,3,-1), (2,0,1)).shape
+# %%
--- 01_U-net/PyTorch/02_Digital_srini/04_multiclass_utils.py
+++ 01_U-net/PyTorch/02_Digital_srini/04_multiclass_utils.py
@@ -4,6 +4,8 @@ from torch.utils.data.dataset import Dataset
 from torchvision import transforms
 import os
 from PIL import Image
+import torch as t
+import numpy as np
 
 class DataSet(Dataset):
     def __init__(self, path_img, path_mask, cstm_transform=None):
@@ -25,13 +27,18 @@ class DataSet(Dataset):
     def __getitem__(self, index):
         s_img = Image.open(os.path.join(self.path_train, self.images[index])).convert('RGB')
         s_msk = Image.open(os.path.join(self.path_mask, self.masks[index]))
-        return self.transform(s_img), self.transform(s_msk)
+        numpy_img = np.array(s_msk)
+        h,w = numpy_img.shape
+        s_mask_reshaped = numpy_img.reshape((-1))
+        onehot_msk = t.nn.functional.one_hot(t.tensor(s_mask_reshaped, dtype=t.long), 4)
+        original_shape = onehot_msk.reshape(h, w, -1)
+        return self.transform(s_img), np.transpose(original_shape, (2, 0, 1))
 
 
 if __name__ == "__main__":
     path_img = '/Users/dhamodharan/My-Python/AI-Tutorials/dataset/Types of seg/02_Multi/X'
     path_msk = '/Users/dhamodharan/My-Python/AI-Tutorials/dataset/Types of seg/02_Multi/Y'
     data = DataSet(path_img, path_msk)
-    print(dir(data))
-    print(data[1][1])
-    print(len(data))
+    print(f"Options available for Dataset = {dir(data)}")
+    print(f"Shape od the mask image = {data[1][1].shape}")
+    print(f"Length of the data = {len(data)}")
