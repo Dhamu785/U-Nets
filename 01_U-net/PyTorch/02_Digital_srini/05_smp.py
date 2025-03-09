8772b11259218f6190cae365b17c9fb54ebfa724
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-03-10 00:29:55 +0530

    preprocessing

--- 01_U-net/PyTorch/02_Digital_srini/05_smp.py
+++ 01_U-net/PyTorch/02_Digital_srini/05_smp.py
@@ -1,10 +1,27 @@
 # %% import libaries
 import segmentation_models_pytorch as smp
-# %%
-NUM_CLASS = 4
+import torch as t
 
-pre_trained_encoder = "densenet121"
+# %% checks
+print(t.cuda.is_available())
+print(t.version.cuda)
+print(t.backends.cudnn.version())
+print(t.backends.cudnn.enabled)
+
+# %% Declar the variables
+NUM_CLASS = 4
+DEVICE = 'cuda' if t.cuda.is_available() else 'cpu'
+print(f"Available device = {DEVICE}")
+pre_trained_encoder = "resnet101"
 pre_trained_weight = 'imagenet'
+
+# %% Pre-process images
+pre_process = smp.encoders.get_preprocessing_fn(pre_trained_encoder, pre_trained_weight)
+# %%Load pre-trained model
 model = smp.Unet(encoder_name = pre_trained_encoder, encoder_weights=pre_trained_weight,
                     activation='softmax', classes=NUM_CLASS)
-# %%
+
+# %% Pass some random values
+random = t.randn((1024,1024,3))
+prcsd = pre_process(random)
+prcsd.shape
