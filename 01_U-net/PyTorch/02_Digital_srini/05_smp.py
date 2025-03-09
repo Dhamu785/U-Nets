05e0ced27280ad9ac6f3e36732de5d4ca8fe1e50
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-03-09 12:18:11 +0530

    model

--- 01_U-net/PyTorch/02_Digital_srini/05_smp.py
+++ 01_U-net/PyTorch/02_Digital_srini/05_smp.py
@@ -1,3 +1,10 @@
 # %% import libaries
 import segmentation_models_pytorch as smp
 # %%
+NUM_CLASS = 4
+
+pre_trained_encoder = "densenet121"
+pre_trained_weight = 'imagenet'
+model = smp.Unet(encoder_name = pre_trained_encoder, encoder_weights=pre_trained_weight,
+                    activation='softmax', classes=NUM_CLASS)
+# %%
