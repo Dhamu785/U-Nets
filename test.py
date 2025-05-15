6a02d1d31aac1925ae3afdcb008c69cbed8dc95b
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2025-05-15 19:42:17 +0530

    removed unwanted files

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-img_path = r'C:\Users\dhamu\Documents\Python all\alfa-TKG\AI Team\CBIR\Improvement works\Image enhancement\version-2\splits\DD\combained\data\low quality\copy\data'
-files = os.listdir(img_path)
-count = 0
-for i in files:
-    os.rename(os.path.join(img_path, i), os.path.join(img_path, str(count)+'.jpeg'))
-    count += 1
