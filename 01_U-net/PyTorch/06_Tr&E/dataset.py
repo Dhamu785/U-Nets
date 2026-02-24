33966c57a6d0cecd8c3c18f250a8c51c70d01263
Dhamodharan <76835665+Dhamu785@users.noreply.github.com>
2026-02-24 19:27:26 +0530

    dataset update

--- 01_U-net/PyTorch/06_Tr&E/dataset.py
+++ 01_U-net/PyTorch/06_Tr&E/dataset.py
@@ -17,30 +17,21 @@ class seg_dataset(Dataset):
             self.images = sorted([os.path.join(path, "Clean", i) for i in os.listdir(os.path.join(path, "Clean"))])
             self.labels = sorted([os.path.join(path, "Noisy", i) for i in os.listdir(os.path.join(path, "Noisy"))])
 
-        # self.transforms = transforms.Compose([
-        #     transforms.Resize((512,512)),
-        #     transforms.ToTensor()
-        # ])
-
         self.transforms = A.Compose([
-            A.Resize(512, 512, interpolation=1),  # cv2.INTER_LINEAR for image
+            A.Resize(512, 512),  # cv2.INTER_LINEAR for image
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
             A.RandomRotate90(p=0.5),              # handles 0/90/180/270
-            A.GaussianBlur(blur_limit=(5, 5), sigma_limit=(1.5, 1.5), p=0.5),  # image-only
-            A.Resize(512, 512, interpolation=1),
+            A.GaussianBlur(blur_limit=3, p=0.2),
             ToTensorV2()
-        ], additional_targets={'mask': 'mask'}, is_check_shapes=False)
+        ], additional_targets={'mask': 'mask'}, is_check_shapes=True)
 
     def __getitem__(self, index):
         img = np.array(Image.open(self.images[index]).convert('RGB'))
         mask = np.array(Image.open(self.labels[index]).convert('L'))
         aug = self.transforms(image=img, mask=mask)
-        # img = Image.open(self.images[index]).convert('RGB')
-        # mask = Image.open(self.labels[index]).convert('L')
 
         return aug['image']/255.0, aug['mask'].unsqueeze(0)/255.0
-        # return self.transforms(img), self.transforms(mask)
 
     
     def __len__(self):
